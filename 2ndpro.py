492f8ae18cd90d7febae855ec052874ffd2b11eb
NasirHussainSajidi <nasirsajidi313@gmail.com>
2025-09-11 18:22:00 +0500

    class 2

---
+++ 2ndpro.py
@@ -0,0 +1,23 @@
+print("write a program by using string")
+name = "Nasir Hussain"
+age = 18
+NIC = 71102-3650484-9
+print("Name is ", name ,"and age is ",age , "and NIC is ", NIC)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+print(name[9])
+print(name[10])
+print(name[11])
+print(name[12])
+
+city = "karachi"
+print(city[:3])
+print(city[0:-1])
+print(city[-3:-1])
